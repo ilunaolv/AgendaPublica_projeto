12da26faa7f310d49673be43b265e0ea33c22f13
Juliana Oliveira <julianalo@praiagrande.sp.gov.br>
2025-11-18 17:55:49 -0300

    Changes: adicionando melhorias no dashboard e na pagina de relatorios. Add: pagina de notificacoes (andamento...)

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+        public IActionResult notificacoes()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
